3f335fd76f3886e45701be65e38c8a3eee5bb6b5
Mike <mike@prime31.com>
2013-07-06 23:58:05 -0700

    removed mecanim vars from non mecanim state

--- Assets/StateKit/SKState.cs
+++ Assets/StateKit/SKState.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 
 public abstract class SKState<T,U>
 {
-	internal int mecanimStateHash;
 	protected SKStateMachine<T,U> _machine;
 	protected T _context;
 	virtual public U stateId
@@ -13,25 +12,9 @@ public abstract class SKState<T,U>
 		get { throw new System.Exception( "stateId property not defined in child class" ); }
 	}
 	
-	
+
 	public SKState()
 	{}
-	
-	
-	/// <summary>
-	/// constructor that takes the mecanim state name as a string
-	/// </summary>
-	public SKState( string mecanimStateName ) : this( Animator.StringToHash( mecanimStateName ) )
-	{}
-	
-	
-	/// <summary>
-	/// constructor that takes the mecanim state hash
-	/// </summary>
-	public SKState( int mecanimStateHash )
-	{
-		
-	}
 
 	
 	public void setMachineAndContext( SKStateMachine<T,U> machine, T context )
